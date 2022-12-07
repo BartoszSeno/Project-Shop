0d33f994044771181f63e09927492fa88ee1a38a
Bartosz <nasieniu@gmail.com>
2022-12-07 11:30:32 +0100

    header menu for shop

--- src/shop/homeHeader/header.js
+++ src/shop/homeHeader/header.js
@@ -1,32 +1,25 @@
+import Option from "./option/option";
+
 function ShopHeader() {
   return (
     <>
-      <header className="home-header-container">
+      <header className="shop-header-container">
         <form className="form-h-h">
-          <input type="text" className="search-home" />
-          <label htmlFor="price" className="form-h-l">
+          <section>
+            <label htmlFor="price" className="form-h-l-s">
+              Search
+            </label>
+            <input type="text" className="search-shop" />
+          </section>
+          <label htmlFor="price" className="form-h-l-p">
             Price
           </label>
-          <div className="price-c">
+          <section className="section-price">
             <input type="number" className="price" />
             <span className="betwen-price">-</span>
             <input type="number" className="price" />
-          </div>
-          <label htmlFor="country" className="form-h-l">
-            Country
-          </label>
-          <select
-            id="country"
-            name="countrylist"
-            form="countryform"
-            className="country-s"
-          >
-            <option value="all Country">All Country</option>
-            <option value="Poland">Poland</option>
-            <option value="Global">Global</option>
-            <option value="Europe">Europe</option>
-            <option value="Asia">Asia</option>
-          </select>
+          </section>
+          <Option />
         </form>
       </header>
     </>
