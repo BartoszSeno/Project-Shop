83ace6cbcc32099ebe1c77d7bf9866d59c7ef2f8
Bartosz <nasieniu@gmail.com>
2022-12-15 22:42:24 +0100

    fix burger menu

--- src/burger/burgernav.js
+++ src/burger/burgernav.js
@@ -2,20 +2,25 @@ import { Link } from "react-router-dom";
 import Logo from "../header/logo/logo";
 
 function BurgerNavi({ burgers }) {
+  const refresh = async () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 50);
+  };
   return (
     <>
       <nav className="burger-nav" style={{ display: burgers ? "flex" : "" }}>
-        <Link to="/" className="logo-b">
+        <Link to="/" className="logo-b" onClick={refresh}>
           <Logo />
         </Link>
         <div className="nav-container">
-          <Link to="/Shop" className="b-a">
+          <Link to="/Shop" className="b-a" onClick={burgers}>
             <div className="b-shop">Shop</div>
           </Link>
-          <Link to="/Pc" className="b-a">
+          <Link to="/Pc" className="b-a" onClick={burgers}>
             <div className="b-pc">Computer Games</div>
           </Link>
-          <Link to="/Ps" className="b-a">
+          <Link to="/Ps" className="b-a" onClick={burgers}>
             <div className="b-psn">PlayStation</div>
           </Link>
         </div>
